f26d89299a57d1ee3b872cc141bb09f79a84bc67
GanYihuan <583520052@qq.com>
2021-11-13 01:21:06 +0800

    50

---
+++ qiuzhi/src/com/qiuzhi50constructionmethod/Person.java
@@ -0,0 +1,23 @@
+package com.qiuzhi50constructionmethod;
+
+public class Person {
+  int age = 0;
+  String name = "";
+
+  public Person(int a, String n) {
+    age = a;
+    name = n;
+  }
+
+  public Person(int a) {
+
+  }
+
+  public Person(String n) {
+
+  }
+
+  public void showInfo() {
+    System.err.println(name + age);
+  }
+}
---
+++ qiuzhi/src/com/qiuzhi50constructionmethod/Test_01.java
@@ -0,0 +1,10 @@
+package com.qiuzhi50constructionmethod;
+
+public class Test_01 {
+  public static void main(String[] args) {
+    Person p1 = new Person(28, "ganyihuan");
+    p1.showInfo();
+    new Person(1);
+    new Person("ganyihuan");
+  }
+}
