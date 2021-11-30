3295bf34cb3f9561e236e400210aaf4aba92a1f4
GanYihuan <583520052@qq.com>
2021-11-30 21:52:53 +0800

    40

--- qiuzhi/src/com/qiuzhi40OOP/Test_B.java
+++ qiuzhi/src/com/qiuzhi40OOP/Test_B.java
@@ -1,16 +1,16 @@
 package com.qiuzhi40OOP;
 
 public class Test_B {
-  static int t;
+  static int t; // static 方法里, 成员变量需要 static 修饰
 
   public static void main(String[] args) {
-    Person p = new Person(); // 类的实例化 new 创建对象
-    p.name = "zhangsha";
-    System.out.println(Person.sex); // static 不需要实例化就可以使用, 直接 . 调用
-    p.showName("lisi");
+    Person p = new Person(); // 类的实例化
 
+    p.name = "zhangsha";
+    System.out.println(Person.sex); // static 对象调用
+    p.showName("lisi");
     p.age = 11;
     t = p.getAge();
-    System.err.println(t); // static 方法里只能访问 static 成员变量
+    System.err.println(t);
   }
 }
